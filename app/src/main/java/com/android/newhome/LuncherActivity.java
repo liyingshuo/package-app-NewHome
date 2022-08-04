66071e78a5ead9a475ff672ecc8703ff0df953de
liyingshuo <liyingshuo@xiaomi.com>
2022-08-04 17:13:49 +0800

    hide actionbar and add splash avtivity
    Signed-off-by: liyingshuo <liyingshuo@xiaomi.com>

---
+++ app/src/main/java/com/android/newhome/LuncherActivity.java
@@ -0,0 +1,32 @@
+package com.android.newhome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class LuncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_luncher);
+        //隐藏标签栏
+        getSupportActionBar().hide();
+
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LuncherActivity.this, MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                finish();
+            }
+        };
+        timer.schedule(timerTask,2000);
+    }
+}
